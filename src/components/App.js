2ba0d5b2ee4b6506414d03fc1e3c13019cd2a19d
Shota <manmail922@gmail.com>
2021-09-15 18:41:33 +0900

    create event form and event table with bootstrap

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,45 @@
 import React, {useEffect, useState } from "react";
 
-const App = props => {
-  const [state, setState] = useState(props);
-  const reset=()=>setState(props)
-  const {name, price}=state
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-  useEffect(()=>{
-    console.log("useEffect is invoked")
-  })
-  useEffect(()=>{
-    console.log("This is like componentDidMount")
-  }, [])
-  useEffect(()=>{
-    console.log("This callback is for name only")
-  }, [name])
-
-  const renderPeriod=()=>{
-    console.log("renderPeriod renders period")
-    return "。"
-  }
+const App = () => {
 
   return (
     <>
-      <p>
-        現在の{name}は{price}円です{renderPeriod()}
-      </p>
-      <button onClick={() => setState({...state, price:price + 1})}>+1</button>
-      <button onClick={() => setState({...state, price:price - 1})}>-1</button>
-      <button onClick={reset}>Reset</button>
-      <input value={name} onChange={e => setState({...state, name: e.target.value})} />
+    <div className="container-fluid"> 
+      <h4>イベント作成フォーム</h4>
+      <form>
+        <div className="form-group">
+          <label htmlFor="formEventTitle">タイトル</label>
+          <input className="form-control" id="formEventTitle"/>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="formEventBody">ボディー</label>
+          <textarea className="form-control" id="formEventBody"/>
+        </div>
+
+        <button className="btn btn-primary">イベントを作成する</button>
+        <button className="btn btn-danger">全てのイベントを削除する</button>
+      </form>
+
+      <h4>イベント一覧</h4>
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>タイトル</th>
+            <th>ボディー</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+      </table>
+    </div>
     </>
   );
-};
-
-// 初期値が外部から与えられると仮定
-App.defaultProps = {
-  name: "",
-  price: 1000
 };
 
 export default App;
