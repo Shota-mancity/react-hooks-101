34d5b846653178ff5db83b258ca01c2d06f973d4
Shota <manmail922@gmail.com>
2021-09-24 11:57:58 +0900

    save state with localStorage

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,37 @@
-import React, { useReducer } from "react";
+import React, { useEffect, useReducer } from "react";
 import reducer from "../reducers";
 import EventForm from "./EventForm";
 import Events from "./Events";
-import OperationLogs from "./OperationLogs"
+import OperationLogs from "./OperationLogs";
 import AppContext from "../contexts/AppContext";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import operationLogs from "../reducers/operationLogs";
+
+const APP_KEY = "appWithRedux";
 
 const App = () => {
-  const initialState={
-    events:[],
-    operationLogs:[]
-  }
+  // localStorageに保存された内容を取り出し、何も入っていなければnullが返ってくる
+  const appState = localStorage.getItem(APP_KEY);
+
+  const initialState = appState
+    ? JSON.parse(appState)
+    : {
+        events: [],
+        operationLogs: []
+      };
+  // リロード時のstate初期化
   const [state, dispatch] = useReducer(reducer, initialState);
+
+  // stateを監視して、stateが更新されるたびにその内容をlocalStorageに保存する
+  useEffect(() => {
+    // localStorageに保存できるのは文字列のため、JSON.stringifyでstateを文字列化
+    localStorage.setItem(APP_KEY, JSON.stringify(state));
+  }, [state]);
 
   return (
     <>
-    {/* 既存のDOMをproviderでラップしてあげる */}
-      <AppContext.Provider value={{state, dispatch}}>   
+      {/* 既存のDOMをproviderでラップしてあげる */}
+      <AppContext.Provider value={{ state, dispatch }}>
         <div className="container-fluid">
           <EventForm />
           <Events />
